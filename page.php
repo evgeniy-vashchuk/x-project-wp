161b6b9af98b771bee34ebffa6c6d33af3d5c9e1
evgeniy-vashchuk <evgeniy.vashchuk.mail@gmail.com>
2026-04-27 20:14:36 +0300

    Add flexible layout support in page.php and update theme metadata in style.css. Updated screenshot.png.

--- page.php
+++ page.php
@@ -2,6 +2,9 @@
 
 get_header();
 
+global $flexible_layouts;
+$flexible_layouts = array();
+
 if (is_plugin_active('woocommerce/woocommerce.php') && is_cart()):
 
   if (have_posts()):
@@ -14,6 +17,9 @@ else:
 
   if ( have_rows('content') ):
     while ( have_rows('content') ) : the_row();
+      $layout = get_row_layout();
+      $flexible_layouts[] = $layout;
+
       get_template_part( 'partials/flexible-sections/' . get_row_layout() );
     endwhile;
   endif;
